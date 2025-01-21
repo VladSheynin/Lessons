913d8749bf5247b27da1305f545b3ed5486d097e
V.Sheynin <sheynin_vlad@rambler.ru>
2025-01-21 13:19:33 +0300

    implenemtation method moveAndMergeEqual in GameHepler class

--- src/main/java/GameHelper.java
+++ src/main/java/GameHelper.java
@@ -9,18 +9,31 @@ import java.util.List;
 public class GameHelper {
     /**
      * Реализация обработки последовательности значений. Соседние склеиваются
-     * @param list
-     * @return
+     * @param list - входной список для сравнения
+     * @return итоговый список одинакового с входящим размера, добитый null-ами при склеивании. null-ы перенесены в конец списка
      */
     List<Integer> moveAndMergeEqual(List<Integer> list)
     {
-        List resultList = new ArrayList();
-        for (Integer item:list)
-        {
-            
-        }
-
-
+    int sizeNewlist=0;
+    List<Integer> resultArrayList = new ArrayList<>();
+    //заполняю значениями которые не null
+        for (int i=0;i<list.size();i++)
+            if (list.get(i)!=null)
+            {
+                resultArrayList.add(list.get(i));sizeNewlist++;
+            }
+        //провожу объединение одинаковых элементов, удаляя один из них
+        int count = 0;
+        for(int i=0;i<sizeNewlist-count;i++)
+            if (resultArrayList.get(i)==resultArrayList.get(i+1))
+            {
+                resultArrayList.set(i+1,resultArrayList.get(i)*2);
+                resultArrayList.remove(i); count++;
+            }
+        //добиваю размер итогового списка до исходного null-ами
+        for (int i = 0; i < list.size()-sizeNewlist; i++)
+            resultArrayList.add(null);
+    return resultArrayList;
     }
 
 }
--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestClass {
     public static void main(String[] args) {
         Board board = new SquareBoard(4);
         Game game2048 = new Game2048(board);
         System.out.println(game2048.canMove());
+
     }
+
+
 }
